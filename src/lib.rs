f1621bf02497874054421044145b4cc9408c727a
John P Mayer <john.p.mayer.jr@gmail.com>
2015-02-21 22:58:58 -0500

    i think this will parse lines of assembly now?

--- src/lib.rs
+++ src/lib.rs
@@ -91,10 +91,39 @@ pub mod lc4 {
     
     use lc4::architecture::*;
     
-    type Label = String;
+    pub type Label = String;
     
-    enum Assembly { 
-      Insn(Insn),
+    pub enum Assm { 
+      NOP,
+      BR(CC,Label),
+      ADD(RName, RName, RName),
+      MUL(RName, RName, RName),
+      SUB(RName, RName, RName),
+      DIV(RName, RName, RName),
+      ADDi(RName, RName, IMM5),
+      CMP(RName, RName),
+      CMPu(RName, RName),
+      CMPi(RName, IMM7),
+      CMPiu(RName, UIMM7),
+      JSR(Label),
+      JSRr(RName),
+      AND(RName, RName, RName),
+      NOT(RName, RName),
+      OR(RName, RName, RName),
+      XOR(RName, RName, RName),
+      ANDi(RName, RName, IMM5),
+      LDR(RName, RName, IMM6),
+      STR(RName, RName, IMM6),
+      RTI,
+      CONST(RName, IMM9),
+      SLL(RName, RName, UIMM4),
+      SRA(RName, RName, UIMM4),
+      SRL(RName, RName, UIMM4),
+      MOD(RName, RName, RName),
+      JMPr(RName),
+      JMP(Label),
+      HICONST(RName, UIMM8),
+      TRAP(UIMM8),
       RET,
       LEA(RName, Label),
       LC(RName, Label),
@@ -103,9 +132,10 @@ pub mod lc4 {
       ADDR(UIMM16),
       FALIGN,
       FILL(IMM16),
-      BLK(UIMM16),
-      CONST(Label, IMM16),
-      UCONST(Label, UIMM16)
+      STRINGZ(String),
+      BLKW(UIMM16),
+      LCONST(Label, IMM16),
+      LUCONST(Label, UIMM16)
     }    
     
     peg_file! modname("lc4_assembly.rustpeg");
