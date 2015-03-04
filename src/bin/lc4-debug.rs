5970fc3248ba73e841f6195f8b0e617ffe84388a
John P Mayer <john.p.mayer.jr@gmail.com>
2015-03-03 23:19:57 -0500

    pretty printing cpu as prereq for step simulation

--- src/bin/lc4-debug.rs
+++ src/bin/lc4-debug.rs
@@ -1,11 +1,24 @@
-#![feature(env)]
+#![feature(core, env)]
 
 extern crate lc4;
 
+use std::cmp::*;
 use std::env::args;
+use std::iter::range;
 
 use lc4::assm_data::*;
 use lc4::processor::*;
+
+fn print_proc(cpu: &CPU) -> () {
+  println!("Registers {:?} NZP {} PC {}", cpu.regfile, cpu.nzp, cpu.pc);
+  let radius = 3;
+  let low = max(0, cpu.pc as i32 - radius) as usize;
+  let high = min(cpu.pc as i32 + radius + 1, cpu.memory.len() as i32) as usize;
+  println!("{} {}", low, high);
+  for i in range(low, high) {
+    println!("{} {:#04x} {}", if i == cpu.pc as usize {"*"} else {" "}, i, cpu.memory[i]);
+  }
+}
 
 pub fn main() -> () {
   let ref source_file: String = match args().nth(1) {
@@ -23,14 +36,14 @@ pub fn main() -> () {
 
   let mut cpu = boot(assm_data);
 
-  println!("Registers {:?} PC {} NZP {}", cpu.regfile, cpu.pc, cpu.nzp);
+  print_proc(&cpu);
 
   for _ in [0;3].iter() {
     match cpu.step() {
       Err(err) => panic!("{:?}", err),
       Ok(()) => ()
     }
-    println!("Registers {:?} PC {} NZP {}", cpu.regfile, cpu.pc, cpu.nzp);
+    print_proc(&cpu);
   }
   
 }
