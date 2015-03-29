051d08e9c12a1027e570b0e04453dca8faca54b4
John P Mayer Jr <john.p.mayer.jr@gmail.com>
2015-03-29 14:04:10 -0400

    wip

--- src/assm_data.rs
+++ src/assm_data.rs
@@ -1,5 +1,8 @@
 use std::collections::HashMap;
-use std::old_io::{File, FileAccess, FileMode, IoResult};
+use std::io;
+use std::old_io::{FileAccess, FileMode, Reader, Writer};
+use std::fs::{File, OpenOptions};
+use std::path::Path;
 
 use architecture::*;
 use encoder::*;
@@ -11,44 +14,46 @@ pub enum Section { CODE, DATA }
 
 #[derive(Clone, Copy, Debug)]
 pub enum Mem {
-  CODE(Insn),
-  DATA(i16)
+    CODE(Insn),
+    DATA(i16)
 }
 
 pub type Memory<M> = Box<[M;0x10000]>;
 
 pub struct AssmData<M> {
-  pub memory: Memory<M>,
-  pub labels: HashMap<Label, (Section, u16)>,
-  pub heap: u16
+    pub memory: Memory<M>,
+    pub labels: HashMap<Label, (Section, u16)>,
+    pub heap: u16
 }
 
 pub fn encode_word(mem: Mem) -> i16 {
-  match mem {
-    Mem::CODE(insn) => encode_insn(insn),
-    Mem::DATA(i) => i
-  }
+    match mem {
+        Mem::CODE(insn) => encode_insn(insn),
+        Mem::DATA(i) => i
+    }
 }
 
-pub fn write_object_file(assm_data: AssmData<Mem>, out_file: &str) -> IoResult<()> {
-  let mut file = try!(File::open_mode(&Path::new(out_file), FileMode::Truncate, FileAccess::Write));
-  try!(file.write_be_u16(assm_data.heap));
-  for addr in 0..assm_data.heap {
-    try!(file.write_be_i16(encode_word(assm_data.memory[addr as usize])))
-  }
-  Ok(())
+pub fn write_object_file(assm_data: AssmData<Mem>, out_file: &str) -> Result<(), io::Error> {
+    let mut options = OpenOptions::new().read(true).write(true).truncate(true);
+    let mut file = try!(options.open(&Path::new(out_file)));
+    try!(file.write_be_u16(assm_data.heap));
+    for addr in 0..assm_data.heap {
+        try!(file.write_be_i16(encode_word(assm_data.memory[addr as usize])))
+    }
+    Ok(())
 }
 
-pub fn read_object_file(in_file: &str) -> IoResult<AssmData<i16>> {
-  let mut file = try!(File::open(&Path::new(in_file)));
-  let heap: u16 = try!(file.read_be_u16());
-  let mut memory: Memory<i16> = box [0;0x10000];
-  for addr in 0..heap {
-    memory[addr as usize] = try!(file.read_be_i16());
-  }
-  Ok(AssmData{
-    memory: memory,
-    labels: HashMap::new(),
-    heap: heap
-  })
+pub fn read_object_file(in_file: &str) -> Result<AssmData<i16>, io::Error> {
+    let mut options = OpenOptions::new().read(true);
+    let mut file = try!(options.open(&Path::new(in_file)));
+    let heap: u16 = try!(file.read_be_u16());
+    let mut memory: Memory<i16> = box [0;0x10000];
+    for addr in 0..heap {
+        memory[addr as usize] = try!(file.read_be_i16());
+    }
+    Ok(AssmData{
+        memory: memory,
+        labels: HashMap::new(),
+        heap: heap
+    })
 }
