cd22e58f981189d870abc6b16523fae65c5b1dae
John P Mayer Jr <john.p.mayer.jr@gmail.com>
2015-07-18 16:37:37 -0400

    bump peg, clean up some features

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 
 #![crate_name = "lc4"]
 #![crate_type = "rlib"]
-#![feature(box_syntax, collections, plugin, str_char)]
+#![feature(box_syntax, plugin)]
 #![plugin(peg_syntax_ext)]
 
 //extern crate core;
